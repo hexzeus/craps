310188c00745aea7c95429051cb5abc2c9ca0871
adam ives <hex.zeus@yahoo.com>
2024-07-16 09:59:09 -0400

    Normalize all the line endings

--- craps/src/App.js
+++ craps/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, NavbarBrand, NavbarLinks, NavLink, Footer } from './AppStyles';
 import Home from './components/Home';
 import Schedule from './components/Schedule';
