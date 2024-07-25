4eacf37bf87fd592ec97c3cecc3147a47ec808bd
adam ives <hex.zeus@yahoo.com>
2024-07-24 20:51:41 -0400

    Update application

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GlobalStyle, Navbar, NavbarBrand, NavbarLinks, NavLink, Footer, Logo, MainImage } from './AppStyles';
 import Home from './components/Home';
@@ -17,47 +17,61 @@ import logo from './images/logo.png';
 import mainImage from './images/main.jpg';
 
 function App() {
+  const logoRef = useRef(null);
+
+  useEffect(() => {
+    if (logoRef.current) {
+      logoRef.current.click();
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <ScrollToTop />
+      <header>
+        <Navbar>
+          <NavbarBrand to="/index.html" ref={logoRef}>
+            <Logo src={logo} alt="Buffalo Creek Craps Logo" />
+            BCCC
+          </NavbarBrand>
+          <NavbarLinks>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/schedule">Class Schedule</NavLink>
+            <NavLink to="/tips">Tips & Resources</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </NavbarLinks>
+        </Navbar>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/tips" element={<Tips />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/interactive-tool1" element={<InteractiveTool1 />} />
+          <Route path="/interactive-tool2" element={<InteractiveTool2 />} />
+          <Route path="/interactive-tool3" element={<InteractiveTool3 />} />
+          <Route path="/interactive-tool4" element={<InteractiveTool4 />} />
+          <Route path="/interactive-tool5" element={<InteractiveTool5 />} />
+          <Route path="/interactive-tool6" element={<InteractiveTool6 />} />
+          <Route path="/interactive-tool7" element={<InteractiveTool7 />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <MainImage src={mainImage} alt="Main Educational Image" />
+      <Footer>
+        <p>&copy; 2024 Buffalo Creek Craps Class. All rights reserved.</p>
+      </Footer>
+    </div>
+  );
+}
+
+function AppWrapper() {
   return (
     <Router>
-      <div className="App">
-        <GlobalStyle />
-        <ScrollToTop />
-        <header>
-          <Navbar>
-            <NavbarBrand to="/index.html">
-              <Logo src={logo} alt="Buffalo Creek Craps Logo" />
-              BCCC
-            </NavbarBrand>
-            <NavbarLinks>
-              <NavLink to="/">Home</NavLink>
-              <NavLink to="/schedule">Class Schedule</NavLink>
-              <NavLink to="/tips">Tips & Resources</NavLink>
-              <NavLink to="/contact">Contact</NavLink>
-            </NavbarLinks>
-          </Navbar>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/schedule" element={<Schedule />} />
-            <Route path="/tips" element={<Tips />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/interactive-tool1" element={<InteractiveTool1 />} />
-            <Route path="/interactive-tool2" element={<InteractiveTool2 />} />
-            <Route path="/interactive-tool3" element={<InteractiveTool3 />} />
-            <Route path="/interactive-tool4" element={<InteractiveTool4 />} />
-            <Route path="/interactive-tool5" element={<InteractiveTool5 />} />
-            <Route path="/interactive-tool6" element={<InteractiveTool6 />} />
-            <Route path="/interactive-tool7" element={<InteractiveTool7 />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-        <MainImage src={mainImage} alt="Main Educational Image" />
-        <Footer>
-          <p>&copy; 2024 Buffalo Creek Craps Class. All rights reserved.</p>
-        </Footer>
-      </div>
+      <App />
     </Router>
   );
 }
 
-export default App;
+export default AppWrapper;
